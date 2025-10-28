e6735263c601967a04ca3e6ac11da74a897c56fe
PATHAKAAKASH19 <pathakaakash8900@gmail.com>
2025-10-28 16:04:19 +0530

    fixes some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
 
   return (
-    <div className=" bg-[#272C31] w-full px-15 py-22 max-lg:px-4 h-fit max-xl:px-4 max-2xl:px-6 transition-all duration-700 max-lg:py-30">
+    <div className=" bg-[#272C31] w-full px-15 py-22 max-lg:px-4 h-fit max-xl:px-4 max-2xl:px-6 transition-all duration-700 max-lg:py-30 max-2xl:h-screen">
       <div className="grid grid-cols-2 gap-10 justify-center items-center max-lg:grid-cols-1  max-2xl:gap-4">
         <div className="w-full  row-span-2 h-full rounded-[2em] bg-[#565758] max-lg:hidden"></div>
         <div>
